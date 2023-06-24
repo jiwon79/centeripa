d5745e5a0be6e284cdf1418733fb612474ae81ba
jiwon79 <19-079@ksa.hs.kr>
2023-06-25 00:23:41 +0900

    Scroll animation 추가

--- centeripa/wwwroot/js/site.js
+++ centeripa/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿const startAnimation = (entries, observer) => {
+﻿// show animation
+const startAnimation = (entries, observer) => {
     entries.forEach(entry => {
         if(entry.isIntersecting) {
             entry.target.classList.add("slide-in-from-right");
@@ -13,4 +14,35 @@ const options = { root: null, rootMargin: '0px', threshold: 1 };
 const elements = document.querySelectorAll('.appear-left');
 elements.forEach(el => {
     observer.observe(el, options);
+});
+
+// scroll transform animation
+const scrollElements = document.querySelectorAll('.scroll-transform');
+
+let ticking = false;
+
+const transformByScroll = () => {
+    scrollElements.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        const elemTop = rect.top;
+        const elemBottom = rect.bottom;
+        const isVisible = (elemTop >= 0 && elemTop <= window.innerHeight) || (elemBottom >= 0 && elemBottom <= window.innerHeight);
+
+        if(isVisible) {
+            let distanceFromTop = (window.innerHeight - elemTop) / 5;
+            el.style.transform = `translateX(${distanceFromTop}px)`;
+        }
+    });
+}
+transformByScroll();
+
+window.addEventListener('scroll', function(e) {
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            transformByScroll();
+            ticking = false;
+        });
+
+        ticking = true;
+    }
 });
