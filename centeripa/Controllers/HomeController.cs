2df8a059419de5792eb57f80d8675d338ed99e8f
jiwon79 <19-079@ksa.hs.kr>
2023-07-04 00:23:48 +0900

    [Feature] Home - Member page 추가

--- centeripa/Controllers/HomeController.cs
+++ centeripa/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Member()
     {
         return View();
     }
